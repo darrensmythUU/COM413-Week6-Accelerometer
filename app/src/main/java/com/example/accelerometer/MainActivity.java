d819ae0aad12b268dfba884b7e5f30c728ca2601
Kiphea <djsmyth05@gmail.com>
2025-12-11 08:18:16 +0000

    Using the Accelerometor Sensor

--- app/src/main/java/com/example/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/accelerometer/MainActivity.java
@@ -2,13 +2,106 @@ package com.example.accelerometer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    TextView sensorTxt;
+    private SensorManager sensorManager;
+    private boolean color = false;
+    private View view;
+    private long lastUpdateTime;
+    private static float SHAKE_THRESHOLD =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        view = findViewById(R.id.sensor_txt);
+//        view.setBackgroundColor(Color.GREEN);
+        sensorTxt = (TextView) findViewById(R.id.sensor_txt);
+        sensorTxt.setBackgroundColor(Color.GREEN);
+        sensorTxt.setText("Shake me!");
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                sensorManager.SENSOR_DELAY_NORMAL);
+        lastUpdateTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            getAccelerometor(sensorEvent);
+        }
+    }
+
+    private void getAccelerometor(SensorEvent sensorEvent) {
+        float[] values = sensorEvent.values;
+
+        //Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float gX = x / SensorManager.GRAVITY_EARTH;
+        float gY = y / SensorManager.GRAVITY_EARTH;
+        float gZ = z / SensorManager.GRAVITY_EARTH;
+
+        //gForce calcs
+        float gForce = (float)Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+        long currentTime = System.currentTimeMillis();
+        if (gForce >= SHAKE_THRESHOLD){
+            if (currentTime - lastUpdateTime < 200){
+                return;
+            }
+            lastUpdateTime = System.currentTimeMillis();
+
+            sensorTxt.setText("You shook me");
+            Toast.makeText(this, ">2G", Toast.LENGTH_SHORT).show();
+
+            if (color == true){
+                sensorTxt.setBackgroundColor(Color.GREEN);
+            }
+            else{
+                sensorTxt.setBackgroundColor(Color.RED);
+            }
+            color = !color;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
